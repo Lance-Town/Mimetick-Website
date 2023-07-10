87eba3da2e78300422e1af1446d06f04c8241aee
Lance-Town <85811392+Lance-Town@users.noreply.github.com>
2023-07-09 17:17:05 -0700

    Add form submit handling
    - Add function to handle form submit

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ var navbar = document.querySelector(".navBar");
 
 var sticky = navbar.offsetTop;
 
-
 function stickynavbar() {
   console.log(sticky);
   if (document.documentElement.scrollTop >= sticky) {    
@@ -13,3 +12,23 @@ function stickynavbar() {
   }
 }
 window.addEventListener("scroll", stickynavbar);
+
+
+// stop new page from loading when form submit
+window.addEventListener("load", function() {
+  const form = document.getElementById('singupEmail');
+  form.addEventListener("submit", function(e) {
+    e.preventDefault();
+    const data = new FormData(form);
+    const action = e.target.action;
+    fetch(action, {
+      method: 'POST',
+      body: data,
+    })
+    .then(() => {
+      // alert("Success!");
+      form.reset();
+    })
+  });
+});
+
